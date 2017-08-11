c2698869da635304b831bf5d7725924acdcedf7c
Nafissa Hassan <nafissa_92@hotmail.com>
2017-08-11 02:06:50 +0000

    Get weather and temperature from API

--- script.js
+++ script.js
@@ -11,21 +11,29 @@ function getLocation() {
 function showPosition(position) {
     lat=position.coords.latitude;
     lon=position.coords.longitude;
-    x.innerHTML = "Latitude: " + position.coords.latitude + 
-    "<br>Longitude: " + position.coords.longitude; 
+    // x.innerHTML = "Latitude: " + position.coords.latitude + 
+    // "<br>Longitude: " + position.coords.longitude; 
     
     var url="https://api.openweathermap.org/data/2.5/weather?";
     var key="01b347e0bc70514efa39a98ff1f1b6ce";
-    var data={APPID:key,lat:lat, lon:lon};
+    var units="metric";
+    var unitDisplay="Celsius"
+    
+    var data={APPID:key,lat:lat, lon:lon, units:units};
     $.ajax({
         url:url,
         data:data,
         type:"GET",
         success:function(response){
-            console.log(response);
+            var weatherMain=response.weather[0].main;
+            var temp=response.main.temp;
+            x.innerHTML="<p>The weather today is "+weatherMain+" and the temperature is "+temp+" in "+unitDisplay+"</p>";
+            console.log(response.main.temp)
         }
         
     });
+
+    
 }
 
 
