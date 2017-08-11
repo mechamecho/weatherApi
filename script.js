5abc9e22a7cf6091b7562976da1f5e5b45391f3b
Nafissa Hassan <nafissa_92@hotmail.com>
2017-08-11 00:26:36 +0000

    Get API key, and response from weather API

--- script.js
+++ script.js
@@ -14,17 +14,18 @@ function showPosition(position) {
     x.innerHTML = "Latitude: " + position.coords.latitude + 
     "<br>Longitude: " + position.coords.longitude; 
     
-    var url="api.openweathermap.org/data/2.5/weather?";
+    var url="https://api.openweathermap.org/data/2.5/weather?";
     var key="01b347e0bc70514efa39a98ff1f1b6ce";
     var data={APPID:key,lat:lat, lon:lon};
     $.ajax({
         url:url,
         data:data,
+        type:"GET",
         success:function(response){
             console.log(response);
         }
         
-    })
+    });
 }
 
 
